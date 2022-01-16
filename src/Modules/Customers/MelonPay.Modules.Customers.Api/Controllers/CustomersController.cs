dcd256b8a20c36d8290d093686cd87ac265bbd8f
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-16 16:01:39 +0300

    Customers.Api: Add Customers controller and rest file

---
+++ src/Modules/Customers/MelonPay.Modules.Customers.Api/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MelonPay.Shared.Abstractions.Commands;
+using MelonPay.Modules.Customers.Core.Commands;
+
+namespace MelonPay.Modules.Customers.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    internal class CustomersController : ControllerBase
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public CustomersController(ICommandDispatcher commandDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateCustomer command)
+        {
+            await _commandDispatcher.SendAsync(command);
+            return Accepted();
+        }
+    }
+}
