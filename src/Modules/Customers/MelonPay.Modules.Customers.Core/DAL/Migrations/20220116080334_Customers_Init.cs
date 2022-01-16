f1d0e9bc3c8f2db4a7f36bf0c530b8c0ea3f0460
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-16 11:10:23 +0300

    Customers.DAL: Add Init migration

---
+++ src/Modules/Customers/MelonPay.Modules.Customers.Core/DAL/Migrations/20220116080334_Customers_Init.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MelonPay.Modules.Customers.Core.DAL.Migrations
+{
+    public partial class Customers_Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "customers");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                schema: "customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    FullName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Address = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Nationality = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: true),
+                    Identity = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    Notes = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CompletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    VerifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Address",
+                schema: "customers",
+                table: "Customers",
+                column: "Address");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Email",
+                schema: "customers",
+                table: "Customers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_FullName",
+                schema: "customers",
+                table: "Customers",
+                column: "FullName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Identity",
+                schema: "customers",
+                table: "Customers",
+                column: "Identity");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Name",
+                schema: "customers",
+                table: "Customers",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Nationality",
+                schema: "customers",
+                table: "Customers",
+                column: "Nationality");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Notes",
+                schema: "customers",
+                table: "Customers",
+                column: "Notes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers",
+                schema: "customers");
+        }
+    }
+}
