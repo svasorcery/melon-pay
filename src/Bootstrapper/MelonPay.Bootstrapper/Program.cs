ec5ebaf0269312564867992413acf282baaf5d18
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-17 22:24:25 +0300

    Bootstrapper: Add dynamic modules loading on app start; Customers become IModule

--- src/Bootstrapper/MelonPay.Bootstrapper/Program.cs
+++ src/Bootstrapper/MelonPay.Bootstrapper/Program.cs
@@ -1,22 +1,35 @@
 using MelonPay.Shared.Infrastructure;
-using MelonPay.Modules.Customers.Api;
+using MelonPay.Shared.Infrastructure.Modules;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddCustomersModule()
-    .AddModularInfrastructure();
+var configuration = builder.Configuration;
+var environment = builder.Environment;
+var services = builder.Services;
+
+var assemblies = ModuleLoader.LoadAssemblies(configuration);
+var modules = ModuleLoader.LoadModules(assemblies);
+
+services.AddModularInfrastructure(assemblies);
+foreach (var module in modules)
+{
+    module.Register(services);
+}
 
 var app = builder.Build();
 
-app
-    .UseModularInfrastructure()
-    .UseCustomersModule();
-
+app.UseModularInfrastructure();
 app.UseHttpsRedirection();
 app.UseRouting();
+foreach (var module in modules)
+{
+    module.Use(app);
+}
 app.UseAuthorization();
 app.MapControllers();
 app.MapGet("/", () => "MelonPay API");
+
+assemblies.Clear();
+modules.Clear();
 
 app.Run();
---
+++ src/Modules/Customers/MelonPay.Modules.Customers.Api/CustomersModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using MelonPay.Modules.Customers.Core;
+using MelonPay.Shared.Abstractions.Modules;
+
+[assembly: InternalsVisibleTo("MelonPay.Bootstrapper")]
+namespace MelonPay.Modules.Customers.Api
+{
+    internal class CustomersModule : IModule
+    {
+        public string Name => "Customers";
+
+        public void Register(IServiceCollection services)
+        {
+            services.AddCore();
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+        }
+    }
+}
