e9b7d3514a92ddaa84d1760aaf388a3790bd30ef
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-17 14:04:02 +0300

    Shared.Infra: Add ApiControllerBase with query extensions

--- src/Modules/Customers/MelonPay.Modules.Customers.Api/Controllers/CustomersController.cs
+++ src/Modules/Customers/MelonPay.Modules.Customers.Api/Controllers/CustomersController.cs
@@ -1,39 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MelonPay.Shared.Infrastructure.Api;
 using MelonPay.Shared.Abstractions.Dispatchers;
 using MelonPay.Modules.Customers.Core.Commands;
 using MelonPay.Modules.Customers.Core.Queries;
 
 namespace MelonPay.Modules.Customers.Api.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    internal class CustomersController : ControllerBase
+    internal class CustomersController : ApiControllerBase
     {
-        private readonly IDispatcher _dispatcher;
-
-        public CustomersController(IDispatcher dispatcher)
+        public CustomersController(IDispatcher dispatcher) : base(dispatcher)
         {
-            _dispatcher = dispatcher;
         }
 
         [HttpGet("{customerId:guid}")]
         public async Task<IActionResult> Get([FromRoute] GetCustomer query)
-        {
-            var customer = await _dispatcher.QueryAsync(query);
-
-            if (customer is null)
-            {
-                return NotFound();
-            }
-
-            return Ok(customer);
-        }
-
+            => SingleOrNotFound(await Dispatcher.QueryAsync(query, CancellationToken));
 
         [HttpPost]
         public async Task<IActionResult> Post(CreateCustomer command)
         {
-            await _dispatcher.SendAsync(command);
+            await Dispatcher.SendAsync(command, CancellationToken);
             return Accepted();
         }
     }
---
+++ src/Shared/MelonPay.Shared.Infrastructure/Api/ApiControllerBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MelonPay.Shared.Abstractions.Dispatchers;
+
+namespace MelonPay.Shared.Infrastructure.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        protected readonly IDispatcher Dispatcher;
+		protected CancellationToken CancellationToken => HttpContext.RequestAborted;
+
+        protected ApiControllerBase(IDispatcher dispatcher)
+        {
+            Dispatcher = dispatcher;
+        }
+
+        protected IActionResult SingleOrNotFound<T>(T? model)
+            => model is not null
+                ? Ok(model)
+                : NotFound();
+
+        protected IActionResult ManyOrNoContent<T>(IEnumerable<T>? items)
+            => items is not null && items.Any()
+                ? Ok(items)
+                : NoContent();
+    }
+}
