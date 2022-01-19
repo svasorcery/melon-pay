6e29c14b7bf0239ace835e613e99d87c7783b86e
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-19 23:00:11 +0300

    Users: Add module requests public contract

--- src/Modules/Users/MelonPay.Modules.Users.Api/UsersModule.cs
+++ src/Modules/Users/MelonPay.Modules.Users.Api/UsersModule.cs
@@ -2,7 +2,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using MelonPay.Modules.Users.Core;
+using MelonPay.Modules.Users.Core.DTO;
+using MelonPay.Modules.Users.Core.Queries;
 using MelonPay.Shared.Abstractions.Modules;
+using MelonPay.Shared.Infrastructure.Modules;
+using MelonPay.Shared.Abstractions.Dispatchers;
 
 [assembly: InternalsVisibleTo("MelonPay.Bootstrapper")]
 namespace MelonPay.Modules.Users.Api
@@ -18,6 +22,11 @@ namespace MelonPay.Modules.Users.Api
 
         public void Use(IApplicationBuilder app)
         {
+            app.UseModuleRequests()
+                .Subscribe<GetUserByEmail, UserDetailsDto>("users/get",
+                    (query, serviceProvider, cancellationToken) =>
+                        serviceProvider.GetRequiredService<IDispatcher>().QueryAsync(query, cancellationToken)
+                );
         }
     }
 }
