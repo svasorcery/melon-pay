cce3bf7b5725912d7ae7ad2ed8456e3089774a96
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-19 23:04:07 +0300

    Shared.Dispatchers: Extend IDispatcher with event publishing

--- src/Shared/MelonPay.Shared.Abstractions/Dispatchers/IDispatcher.cs
+++ src/Shared/MelonPay.Shared.Abstractions/Dispatchers/IDispatcher.cs
@@ -1,4 +1,5 @@
 ﻿using MelonPay.Shared.Abstractions.Commands;
+using MelonPay.Shared.Abstractions.Events;
 using MelonPay.Shared.Abstractions.Queries;
 
 namespace MelonPay.Shared.Abstractions.Dispatchers
@@ -6,6 +7,7 @@ namespace MelonPay.Shared.Abstractions.Dispatchers
     public interface IDispatcher
     {
         Task SendAsync<TCommand>(TCommand command, CancellationToken cancellationToken = default) where TCommand : class, ICommand;
+        Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default) where TEvent : class, IEvent;
         Task<TResult> QueryAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default) where TResult : class;
     }
 }
--- src/Shared/MelonPay.Shared.Infrastructure/Dispatchers/InMemoryDispatcher.cs
+++ src/Shared/MelonPay.Shared.Infrastructure/Dispatchers/InMemoryDispatcher.cs
@@ -1,5 +1,6 @@
 ﻿using MelonPay.Shared.Abstractions.Dispatchers;
 using MelonPay.Shared.Abstractions.Commands;
+using MelonPay.Shared.Abstractions.Events;
 using MelonPay.Shared.Abstractions.Queries;
 
 namespace MelonPay.Shared.Infrastructure.Dispatchers
@@ -7,18 +8,26 @@ namespace MelonPay.Shared.Infrastructure.Dispatchers
     internal sealed class InMemoryDispatcher : IDispatcher
     {
         private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IEventDispatcher _eventDispatcher;
         private readonly IQueryDispatcher _queryDispatcher;
 
-        public InMemoryDispatcher(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
+        public InMemoryDispatcher(ICommandDispatcher commandDispatcher, IEventDispatcher eventDispatcher, IQueryDispatcher queryDispatcher)
         {
             _commandDispatcher = commandDispatcher;
+            _eventDispatcher = eventDispatcher;
             _queryDispatcher = queryDispatcher;
         }
 
-        Task IDispatcher.SendAsync<TCommand>(TCommand command, CancellationToken cancellationToken)
+        public Task SendAsync<TCommand>(TCommand command, CancellationToken cancellationToken = default)
+            where TCommand : class, ICommand
             => _commandDispatcher.SendAsync(command, cancellationToken);
 
-        public Task<TResult> QueryAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default) where TResult : class
+        public Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
+            where TEvent : class, IEvent
+            => _eventDispatcher.PublishAsync(@event, cancellationToken);
+
+        public Task<TResult> QueryAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default)
+            where TResult : class
             => _queryDispatcher.QueryAsync(query, cancellationToken);
     }
 }
