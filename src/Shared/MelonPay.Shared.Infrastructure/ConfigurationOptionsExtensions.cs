2e42ada17d2c1322a3f49fdc849432ac9f086069
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-20 22:16:23 +0300

    Shared.Infra: Move GetOptions() extensions to public

---
+++ src/Shared/MelonPay.Shared.Infrastructure/ConfigurationOptionsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MelonPay.Shared.Infrastructure
+{
+    public static class ConfigurationOptionsExtensions
+    {
+        public static T GetOptions<T>(this IServiceCollection services, string sectionName) where T : class, new()
+        {
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+            return configuration.GetOptions<T>(sectionName);
+        }
+
+        public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
+        {
+            var options = new T();
+            configuration.GetSection(sectionName).Bind(options);
+            return options;
+        }
+    }
+}
--- src/Shared/MelonPay.Shared.Infrastructure/Extensions.cs
+++ src/Shared/MelonPay.Shared.Infrastructure/Extensions.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MelonPay.Shared.Abstractions.Time;
 using MelonPay.Shared.Infrastructure.Api;
@@ -31,19 +30,5 @@ namespace MelonPay.Shared.Infrastructure
 
         public static IApplicationBuilder UseModularInfrastructure(this IApplicationBuilder app)
             => app;
-
-        public static T GetOptions<T>(this IServiceCollection services, string sectionName) where T : class, new()
-        {
-            using var scope = services.BuildServiceProvider().CreateScope();
-            var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
-            return configuration.GetOptions<T>(sectionName);
-        }
-
-        public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
-        {
-            var options = new T();
-            configuration.GetSection(sectionName).Bind(options);
-            return options;
-        }
     }
 }
