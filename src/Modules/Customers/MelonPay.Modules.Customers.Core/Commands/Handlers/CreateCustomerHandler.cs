195427bd48afff3c74d8d7f71b703bb10e265993
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-21 15:12:52 +0300

    Customers: Change CreateCustomerHandler to take customer data from user via User API client call (use sync approach)

--- src/Modules/Customers/MelonPay.Modules.Customers.Core/Commands/Handlers/CreateCustomerHandler.cs
+++ src/Modules/Customers/MelonPay.Modules.Customers.Core/Commands/Handlers/CreateCustomerHandler.cs
@@ -2,6 +2,8 @@
 using MelonPay.Shared.Abstractions.Time;
 using MelonPay.Shared.Kernel.ValueObjects;
 using MelonPay.Shared.Abstractions.Commands;
+using MelonPay.Modules.Customers.Core.Clients;
+using MelonPay.Modules.Customers.Core.Exceptions;
 using MelonPay.Modules.Customers.Core.Domain.Entities;
 using MelonPay.Modules.Customers.Core.Domain.Repositories;
 
@@ -10,12 +12,14 @@ namespace MelonPay.Modules.Customers.Core.Commands.Handlers
     internal sealed class CreateCustomerHandler : ICommandHandler<CreateCustomer>
     {
         private readonly ICustomerRepository _customerRepository;
+        private readonly IUserApiClient _userApiClient;
         private readonly IClock _clock;
         private readonly ILogger<CreateCustomerHandler> _logger;
 
-        public CreateCustomerHandler(ICustomerRepository customerRepository, IClock clock, ILogger<CreateCustomerHandler> logger)
+        public CreateCustomerHandler(ICustomerRepository customerRepository, IUserApiClient userApiClient, IClock clock, ILogger<CreateCustomerHandler> logger)
         {
             _customerRepository = customerRepository;
+            _userApiClient = userApiClient;
             _clock = clock;
             _logger = logger;
         }
@@ -24,7 +28,24 @@ namespace MelonPay.Modules.Customers.Core.Commands.Handlers
         {
             _ = new Email(command.Email);
 
-            var customer = new Customer(Guid.NewGuid(), command.Email, _clock.CurrentDate());
+            var user = await _userApiClient.GetAsync(command.Email);
+            if (user is null)
+            {
+                return;
+            }
+
+            if (user.Role is not "user")
+            {
+                return;
+            }
+
+            var customerId = user.UserId;
+            if (await _customerRepository.GetAsync(customerId) is not null)
+            {
+                throw new CustomerAlreadyExistsException(customerId);
+            }
+
+            var customer = new Customer(customerId, command.Email, _clock.CurrentDate());
             await _customerRepository.AddAsync(customer);
             _logger.LogInformation($"Created a customer with ID: '{customer.Id}'");
         }
---
+++ src/Modules/Customers/MelonPay.Modules.Customers.Core/Exceptions/CustomerAlreadyExistsException.cs
@@ -0,0 +1,14 @@
+﻿using MelonPay.Shared.Kernel.Exceptions;
+
+namespace MelonPay.Modules.Customers.Core.Exceptions
+{
+    internal class CustomerAlreadyExistsException : MelonPayException
+    {
+        public Guid CustomerId { get; }
+
+        public CustomerAlreadyExistsException(Guid customerId) : base($"Customer with ID: '{customerId}' already exists.")
+        {
+            CustomerId = customerId;
+        }
+    }
+}
